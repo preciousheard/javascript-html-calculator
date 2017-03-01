0f7d934e70f4952d3e63a22ec0cb19a60cdbd686
Precious Heard <pheard@wlapcs.org>
2017-03-01 03:00:51 +0000

    added text input so user can type

--- script.js
+++ script.js
@@ -1,10 +1,15 @@
 function clearDisplay(){
-    document.getElementById("display-bar").innerHTML = "";
+    document.getElementById("display-bar").value = "";
 }  
 
 function display(userInput){
-    document.getElementById("display-bar").innerHTML += userInput;
+    document.getElementById("display-bar").value += userInput;
     
 }
+function calculate(){
+    var answer = eval(document.getElementById("display-bar").value);
+    document.getElementById("display-bar").value = answer;
+}
+
 
 
